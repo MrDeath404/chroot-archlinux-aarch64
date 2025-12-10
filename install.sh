7aa59144fed42c22f2f8c80b8bd8d563713e8317
MrDeath404 <pisarekpatryk404@gmail.com>
2025-12-10 22:46:07 +0100

    fix

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ info "Installing ArchLinux manager for Termux"
 if [ -f "./archlinux" ]; then
     rm -f "./archlinux"
 fi
-wget https://raw.githubusercontent.com/MrDeath404/chroot-archlinux-aarch64/main/archlinux -q --show-progress
+wget https://raw.githubusercontent.com/MrDeath404/chroot-archlinux-aarch64/main/archlinux
 chmod +x ./archlinux
 mv ./archlinux $HOME/../usr/bin
 
