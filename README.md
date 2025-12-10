acd79c35791a1bc283900f9edb5c9ee21af1dd08
MrDeath404 <pisarekpatryk404@gmail.com>
2025-12-10 22:39:41 +0100

    Added automation

---
+++ README.md
@@ -0,0 +1,3 @@
+```sh
+bash <(curl -s https://raw.githubusercontent.com/MrDeath404/chroot-archlinux-aarch64/main/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,25 @@
+success() {
+    echo -e "[\033[33mSUCCESS\033[0m] - $1"
+}
+
+info(){
+    echo -e "[\033[34mINFO\033[0m] - $1"
+}
+
+error(){
+    echo -e "[\033[31mERROR\033[0m] - $1" >&2
+}
+
+info "Installing ArchLinux manager for Termux"
+wget https://raw.githubusercontent.com/MrDeath404/chroot-archlinux-aarch64/main/archlinux -q --show-progress
+chmod +x ./archlinux
+mv ./archlinux $HOME/../usr/bin
+
+if archlinux; then
+    success "ArchLinux manager was successfully installed"
+    info "You can access ArchLinux Manager \"archlinux\" anywhere in your Termux"
+    info "Now you can setup enviroment by \"archlinux setup\""
+else
+    error "Failed to install manager. Try again"
+    exit 1
+fi
